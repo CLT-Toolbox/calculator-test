c13f96402b5777df3b7ef23a715469bd154033c0
WahyuS04 <211210051@student.mercubuana-yogya.ac.id>
2026-06-26 08:38:21 +0700

    Complete assignment

--- type/clt-layer-properties-type.js
+++ type/clt-layer-properties-type.js
@@ -1,5 +1,22 @@
 class CLTLayerPropertiesType {
-    constructor() {
-        
-    }
+  constructor() {
+    this.index       = 0;
+    this.ti          = 0;
+    this.yi          = 0;
+    this.theta       = 0;
+    this.Ei_XX       = 0;
+    this.Gi          = 0;
+
+    this.beff_ti3_12 = 0;
+    this.beff_ti_ai2 = 0;
+    this.ai          = 0;
+    this.EiIi        = 0;
+
+    this.Ai          = 0;
+    this.GR_next     = null;
+    this.gamma       = null;
+    this.ai_eff      = null;
+    this.EiIi_eff    = 0;
+    this.di          = 0;
+  }
 }
--- type/panel-properties-type.js
+++ type/panel-properties-type.js
@@ -1,5 +1,12 @@
 class PanelPropertiesType {
-    constructor() {
-        this.layers = [];
-    }
+  constructor() {
+    /** @type {CLTLayerPropertiesType[]} */
+    this.layers         = [];
+    this.method         = '';      
+    this.EI_eff         = 0;
+    this.totalThickness = 0;
+    this.layerCount     = 0;
+    this.beff           = 0;
+    this.Lref           = null;
+  }
 }
